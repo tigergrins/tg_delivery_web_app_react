9398e7a6c55f59d9c64d3b9bf3e919552fac125c
danila bokun <d.bokun@yandex.ru>
2022-11-13 23:16:05 +0300

    Added routing

---
+++ src/routing.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+import Menu from './pages/menu';
+
+function MainRouter() {
+  return (
+    <Routes>
+      <Route path='/' element={<Menu/>} />
+      <Route path={routes.Menu} element={<Menu/>} />
+    </Routes>
+  )
+}
+
+export default MainRouter
+
+export const history = createBrowserHistory()
+
+const routes: {
+  [key: string]: string;
+} = {
+  Menu: '/menu',
+  Dish: '/dish',
+  Basket: '/basket',
+  Form: '/form'
+}
