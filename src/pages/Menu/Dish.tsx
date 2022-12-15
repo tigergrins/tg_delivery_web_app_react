561304464395da071789baddc59db96dd3004b7c
danila bokun <d.bokun@yandex.ru>
2022-12-15 22:52:46 +0300

    made Dish

--- src/pages/Menu/Dish.tsx
+++ src/pages/Menu/Dish.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from '../../components/Button'
 import { IDish } from '../../store/content'
 
 interface IDishProps {
@@ -11,10 +12,10 @@ function Dish(props: IDishProps) {
     <Container>
       <ImgContainer><img src={props.data.img} alt="photo" /></ImgContainer>
 
-      <div>{props.data.dishTitle}</div>
-      <div>{props.data.weight}</div>
+      <div className={'title'}>{props.data.dishTitle}</div>
+      <div className={'weight'}>{props.data.weight} g.</div>
 
-      <Button>{props.data.price}</Button>
+      <CustomButton onClick={() => {}}>+ {props.data.price}</CustomButton>
     </Container>
   )
 }
@@ -22,13 +23,34 @@ function Dish(props: IDishProps) {
 export default Dish
 
 const Container = styled.div`
+  position: relative;
+  margin: 1rem;
+  padding-bottom: 1rem;
 
+  .title {
+    font-weight: 700;
+    margin-bottom: 0.5rem;
+  }
+
+  .weight{
+    font-size: 0.8rem;
+    font-weight: 300;
+    color: var(--secondary-text);
+  }
 `
 
 const ImgContainer = styled.div`
-
+  width: 9rem;
+  height: 9rem;
+  background-color: var(--color-red);
+  border-radius: 0.3rem;
+  margin-bottom: 0.75rem;
 `
 
-const Button = styled.div`
-
+const CustomButton = styled(Button)`
+  max-width: 5rem;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 0.5rem 1rem;
 `
