4bab24083731c7a1cdea60282f2341455088938a
Danila Bokun <d.bokun@yandex.ru>
2023-03-26 23:11:03 +0400

    Changed SliderItem layout

--- src/components/Slider/SliderItem.tsx
+++ src/components/Slider/SliderItem.tsx
@@ -18,11 +18,16 @@ export default SliderItem
 
 const Container = styled.div`
   display: flex;
-  justify-content: end;
+  justify-content: center;
+  align-items: end;
 
   min-width: 5rem;
   min-height: 5rem;
   background-color: var(--color-red);
   margin: 0 0.5rem;
   text-align: center;
+
+  span {
+    padding-bottom: 0.5rem;
+  }
 `
